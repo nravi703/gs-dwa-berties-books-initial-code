2253ad80cbb7a2e5766860154ff317c0e651f753
Nemaliya Ravindrakumar <nravi002@gold.ac.uk>
2024-11-20 18:28:38 +0000

    change create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -7,3 +7,5 @@ USE berties_books;
 # Create the tables
 CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
 
+CREATE USER IF NOT EXISTS 'berties_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
+GRANT ALL PRIVILEGES ON berties_books.* TO 'berties_books_app'@'localhost';
